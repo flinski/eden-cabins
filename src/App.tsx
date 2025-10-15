d4b20aa3ebc3ef25911c07f655909359773401c5
Vladislav Flinski <flinssski@gmail.com>
2025-10-15 20:54:26 +0300

    feat: create initial pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Dashboard from '@/pages/Dashboard'
+import Bookings from '@/pages/Bookings'
+import Cabins from '@/pages/Cabins'
+import Users from '@/pages/Users'
+import Settings from '@/pages/Settings'
+import Account from '@/pages/Account'
+import Login from '@/pages/Login'
+import PageNotFound from '@/pages/PageNotFound'
+
 export default function App() {
 	return (
-		<div className="leading-text font-inter text-ui-950 bg-ui-100 overflow-hidden antialiased">
-			Hello World
+		<div className="leading-text font-inter text-ui-950 bg-ui-100 overflow-hidden font-medium antialiased">
+			<BrowserRouter>
+				<Routes>
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="bookings" element={<Bookings />} />
+					<Route path="cabins" element={<Cabins />} />
+					<Route path="users" element={<Users />} />
+					<Route path="settings" element={<Settings />} />
+					<Route path="account" element={<Account />} />
+					<Route path="login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	)
 }
