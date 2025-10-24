1da26bd8fd6122372c3c3728f8b7362cb9212ec0
Vladislav Flinski <flinssski@gmail.com>
2025-10-24 21:52:29 +0300

    feat: add breakfast option

--- src/features/check-in-out/CheckingBooking.tsx
+++ src/features/check-in-out/CheckingBooking.tsx
@@ -8,18 +8,21 @@ import Tag from '@/ui/Tag'
 import Checkbox from '@/ui/Checkbox'
 import { formatCurrency } from '@/lib/utils'
 import { useCheckin } from '@/features/check-in-out/useCheckin'
+import { useSettings } from '@/features/settings/useSettings'
 
 export default function CheckingBooking() {
 	const [confirmPaid, setConfirmPaid] = useState(false)
+	const [addBreakfast, setAddBreakfast] = useState(false)
 	const moveBack = useMoveBack()
 	const { isLoading, error, booking } = useBooking()
 	const { isCheckingIn, checkin } = useCheckin()
+	const { isLoading: isLoadingSettings, settings } = useSettings()
 
 	useEffect(() => {
 		setConfirmPaid(booking?.isPaid ?? false)
 	}, [booking?.isPaid])
 
-	if (isLoading) {
+	if (isLoading || isLoadingSettings) {
 		return (
 			<div className="flex items-center justify-center">
 				<Spinner />
@@ -36,6 +39,12 @@ export default function CheckingBooking() {
 		return <div>Booking cound not be loaded</div>
 	}
 
+	if (!settings) {
+		return <div>Settings cound not be loaded</div>
+	}
+
+	const optionalBreakfastPrice = settings.breakfastPrice * booking.numGuests
+
 	const status = booking.status
 
 	const statusToTagName = {
@@ -49,11 +58,22 @@ export default function CheckingBooking() {
 			return
 		}
 
-		checkin(booking.id)
+		if (addBreakfast) {
+			checkin({
+				bookingId: booking.id,
+				breakfast: {
+					hasBreakfast: true,
+					extrasPrice: optionalBreakfastPrice,
+					totalPrice: booking.totalPrice + optionalBreakfastPrice,
+				},
+			})
+		} else {
+			checkin({ bookingId: booking.id })
+		}
 	}
 
 	return (
-		<div className="flex flex-col gap-8">
+		<div className="flex flex-col gap-5">
 			<div className="flex items-center justify-between gap-6">
 				<h1 className="text-h1 leading-heading font-semibold">Check in booking #{booking?.id}</h1>
 				<Tag type={statusToTagName[status]}>{booking.status.replace('-', ' ')}</Tag>
@@ -61,7 +81,22 @@ export default function CheckingBooking() {
 
 			<BookingDataBox booking={booking} />
 
-			<div>
+			{!booking.hasBreakfast && (
+				<div className="bg-ui-50 border-ui-200 rounded-lg border p-4">
+					<Checkbox
+						id="breakfast"
+						checked={addBreakfast}
+						onChange={() => {
+							setAddBreakfast((add) => !add)
+							setConfirmPaid(false)
+						}}
+					>
+						Want to add breakfast for {formatCurrency(optionalBreakfastPrice)}?
+					</Checkbox>
+				</div>
+			)}
+
+			<div className="bg-ui-50 border-ui-200 rounded-lg border p-4">
 				<Checkbox
 					id="confirm"
 					checked={confirmPaid}
@@ -69,13 +104,15 @@ export default function CheckingBooking() {
 					onChange={() => setConfirmPaid((confirm) => !confirm)}
 				>
 					I confirm that {booking.guests.fullName} has paid the total amount of{' '}
-					{formatCurrency(booking.totalPrice)}
+					{!addBreakfast
+						? formatCurrency(booking.totalPrice)
+						: `${formatCurrency(booking.totalPrice + optionalBreakfastPrice)}`}
 				</Checkbox>
 			</div>
 
 			<div className="flex items-center justify-end gap-4">
 				<button
-					disabled={!confirmPaid || isCheckingIn || booking.isPaid}
+					disabled={!confirmPaid || isCheckingIn}
 					onClick={handleCheckin}
 					className="bg-ui-50 border-ui-200 hover:bg-accent-600 hover:text-ui-50 disabled:hover:bg-ui-50 disabled:hover:text-ui-950 cursor-pointer self-end rounded-md border px-3 py-2 disabled:cursor-default disabled:opacity-50"
 				>
--- src/features/check-in-out/useCheckin.ts
+++ src/features/check-in-out/useCheckin.ts
@@ -9,8 +9,17 @@ export function useCheckin() {
 	const navigate = useNavigate()
 
 	const { mutate: checkin, isPending: isCheckingIn } = useMutation({
-		mutationFn: (bookingId: string) =>
-			updateBooking(bookingId, { status: 'checked-in', isPaid: true }),
+		mutationFn: ({
+			bookingId,
+			breakfast,
+		}: {
+			bookingId: string
+			breakfast?: {
+				hasBreakfast: boolean
+				extrasPrice: number
+				totalPrice: number
+			}
+		}) => updateBooking(bookingId, { status: 'checked-in', isPaid: true, ...breakfast }),
 		onSuccess: (data) => {
 			toast.success(`Booking #${data.id} successfully checked in`)
 			queryClient.invalidateQueries({ queryKey: ['booking'] })
